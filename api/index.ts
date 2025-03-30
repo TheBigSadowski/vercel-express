163e7e2bdd940ebbabf7b178d4ca0eadda439612
Joe Sadowski <joeski@stripe.com>
2025-03-30 10:41:36 -0700

    add a webhook handler and clean up PRICE_ID

--- api/index.ts
+++ api/index.ts
@@ -33,8 +33,7 @@ app.post('/create-checkout-session', async (req, res) => {
     const session = await stripe.checkout.sessions.create({
       line_items: [
         {
-          // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-          price: 'price_1R7mMULsME8qSugw52rCyy58',
+          price: PRICE_ID,
           quantity: 1,
         },
       ],
@@ -45,6 +44,30 @@ app.post('/create-checkout-session', async (req, res) => {
     });
   
     res.redirect(303, session.url);
+});
+
+app.post('/webhook', express.json({type: 'application/json'}), (request, response) => {
+    const event = request.body;
+
+    // Handle the event
+    switch (event.type) {
+      case 'payment_intent.succeeded':
+        const paymentIntent = event.data.object;
+        // Then define and call a method to handle the successful payment intent.
+        // handlePaymentIntentSucceeded(paymentIntent);
+        break;
+      case 'payment_method.attached':
+        const paymentMethod = event.data.object;
+        // Then define and call a method to handle the successful attachment of a PaymentMethod.
+        // handlePaymentMethodAttached(paymentMethod);
+        break;
+      // ... handle other event types
+      default:
+        console.log(`Unhandled event type ${event.type}`);
+    }
+
+    // Return a response to acknowledge receipt of the event
+    response.json({received: true});
 });
 
 app.listen(3000, () => console.log('Server ready on port 3000.'));
