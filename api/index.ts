c4024e9cd6766e74135ae4d176f087ffd2916ed2
Joe Sadowski <joeski@stripe.com>
2025-03-30 11:06:01 -0700

    start to wait

--- api/index.ts
+++ api/index.ts
@@ -12,6 +12,8 @@ const app = express();
 const bodyParser = require('body-parser');
 const path = require('path');
 
+const sleep = ms => new Promise(r => setTimeout(r, ms));
+
 // Create application/x-www-form-urlencoded parser
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
@@ -46,24 +48,25 @@ app.post('/create-checkout-session', async (req, res) => {
     res.redirect(303, session.url);
 });
 
-app.post('/webhook', express.json({type: 'application/json'}), (request, response) => {
+app.post('/webhook', express.json({type: 'application/json'}), async (request, response) => {
     const event = request.body;
 
     // Handle the event
     switch (event.type) {
-      case 'payment_intent.succeeded':
-        const paymentIntent = event.data.object;
-        // Then define and call a method to handle the successful payment intent.
-        // handlePaymentIntentSucceeded(paymentIntent);
-        break;
-      case 'payment_method.attached':
-        const paymentMethod = event.data.object;
-        // Then define and call a method to handle the successful attachment of a PaymentMethod.
-        // handlePaymentMethodAttached(paymentMethod);
-        break;
-      // ... handle other event types
-      default:
-        console.log(`Unhandled event type ${event.type}`);
+        case 'checkout.session.completed':
+            console.log('webhook checkout.session.completed: sleeping and then responding');
+            await sleep(8000);
+            console.log('webhook checkout.session.completed: returning from sleep');
+            break;
+
+        case 'product.updated':
+            console.log('webhook product.updated: sleeping and then responding');
+            await sleep(8000);
+            console.log('webhook product.updated: returning from sleep');
+            break;
+
+        default:
+            console.log(`Unhandled event type ${event.type}`);
     }
 
     // Return a response to acknowledge receipt of the event
